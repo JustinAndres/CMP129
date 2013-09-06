83c6b3382c85b2866521efd9df213624b0794d8f
Justin Andres <andres.justin@student.ccm.edu>
2013-09-05 20:14:00 -0400

    Hello World
    Signed-off-by: Justin Andres <andres.justin@student.ccm.edu>

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string myString = "Hello World From String"; //sentence with numbers and letters
+            char myChar= 'a'; //individual characters
+            int myInt = 0; // whole numbers only ex: 5, 6, 8,
+            float myFloat = 5f; //any decimal point 5.6
+            double myDbl = 5; //more precise floats
+            bool myBool = false; //true or false
+            
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            
+            while ( myInt < 11)
+            {
+                Console.WriteLine( myInt++);
+            }
+       
+        }
+
+    }
+}
